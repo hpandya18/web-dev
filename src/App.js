631d1f2cd351a953ce90b27d2b30a954a5d90423
Harsh Pandya <harshpandya@Harshs-MacBook-Pro.local>
2021-09-20 11:36:28 -0400

    a2 html fa21

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,18 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <ul>
+          <li>
+            <a href="./a2/practise/index.html">HTML Practice</a>
+          </li>
+          <li>
+            <a href="./a2/twitter/navigation.html">Twitter clone</a>
+          </li>
+          <li>
+            <a href="./a2/practise/iframe/index.html">IFrame practise</a>
+          </li>
+        </ul>
       </header>
     </div>
   );
